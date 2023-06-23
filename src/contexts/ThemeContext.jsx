63e0d9cd11143d45e35282043a92540e1ebf0df7
IlCabezon <aguscastro2014.ac@gmail.com>
2023-06-23 01:17:15 -0300

    [FX] theme recognizer

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,5 +1,5 @@
 // native
-import { createContext, useEffect, useMemo, useState } from 'react';
+import { createContext, useMemo, useState } from 'react';
 
 // utils
 import { themeSwitcher } from '../utils/theme.utils';
@@ -7,17 +7,14 @@ import { themeSwitcher } from '../utils/theme.utils';
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(localStorage.theme || 'dark');
+  const [theme, setTheme] = useState(localStorage.theme);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    themeSwitcher();
   };
 
   const contextValue = useMemo(() => ({ theme, toggleTheme }), [theme]);
-
-  useEffect(() => {
-    themeSwitcher();
-  }, [theme]);
 
   return (
     <ThemeContext.Provider value={contextValue}>
